38f4c81ab011b15acd2b61e28aa3b7fac34a409a
TolulopeJoshua <babtol235@gmail.com>
2022-11-24 08:31:55 +0100

    touch

--- index.js
+++ index.js
@@ -51,9 +51,7 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
