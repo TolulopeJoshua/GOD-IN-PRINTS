3e1aad232de1d320c587223241a8dfde1f1c5091
TolulopeJoshua <babtol235@gmail.com>
2022-12-23 11:02:44 +0100

    t

--- utils/lib/videos_functions.js
+++ utils/lib/videos_functions.js
@@ -43,7 +43,7 @@ function sortVideos(req) {
         return { name: playlist.name, videos };
     })
     const forKids = userMovies.filter(movie => movie.forKids && movie.embeddable);
-    forKids.length && userPlaylists.push({name: 'For Kids', videos: forKids});
+    userPlaylists.push({name: 'For Kids', videos: forKids});
     const nonEmbeddable = userMovies.filter(movie => !movie.embeddable);
     nonEmbeddable.length && userPlaylists.push({name: 'Watch on Youtube', videos: nonEmbeddable});
 
