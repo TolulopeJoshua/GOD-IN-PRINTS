407163b83d0bb1e975cf6143ecde49e33f4fe276
TolulopeJoshua <babtol235@gmail.com>
2022-12-23 07:27:35 +0100

    tc

--- utils/lib/videos_functions.js
+++ utils/lib/videos_functions.js
@@ -32,7 +32,7 @@ function sortVideos(req) {
     
     const userStatus = req.user?.subscription.status || 'classic';
     let userMovies = orderedByDate.filter((_, index) => index % 100 < limits.videos[userStatus]);
-    userMovies = userMovies.filter(movie => movie.id != 'GykgCvYsNJw' && movie.id != 'E_8cFo_MXpU')
+    // userMovies = userMovies.filter(movie => movie.id != 'GykgCvYsNJw' && movie.id != 'E_8cFo_MXpU')
     const n = userStatus == 'classic' ? 7 : userStatus == 'premium' ? 10 : 9;
     const userFeatures = userMovies.filter(movie => movie.embeddable && movie.availableInCountry && !movie.forKids).sort(() => 0.5 - Math.random()).slice(0, n).concat([null, null, null]).slice(0,10).sort(() => 0.5 - Math.random());
 
