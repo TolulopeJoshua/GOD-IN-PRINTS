dbbdb0624938ab8aa870b28620a2c0efb1d1012c
TolulopeJoshua <babtol235@gmail.com>
2023-03-31 11:17:05 +0100

    tc

--- utils/capitalize.js
+++ utils/capitalize.js
@@ -2,7 +2,7 @@ module.exports = (text) => {
     const separators = [' ', ' \'', '-', '_', '.']
     let words = text.toLowerCase();
     for (let separator of separators) {
-        words = words.split(separator).map(word => word[0]?.toUpperCase() + word.slice(1)).join(separator);
+        words = words.split(separator).map(word => word[0]?.toUpperCase() + word.slice(1)).filter(w => w).join(separator);
     }
     return words;
 }
