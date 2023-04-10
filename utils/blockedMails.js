b44079af9a0cb6e20c9e02b64d0f5c087cc98a62
TolulopeJoshua <babtol235@gmail.com>
2023-04-10 09:40:59 +0100

    t

--- utils/blockedMails.js
+++ utils/blockedMails.js
@@ -1,1 +1,4 @@
-module.exports = ['tchampion2977@stu.pcssd.org','noelsalvador4@gmail.com','esosaosadirector@yahoo.com','jjeo5191@stu.gusd.net','omolayofoluso@yahoo.com']
+module.exports = [
+    'tchampion2977@stu.pcssd.org','noelsalvador4@gmail.com','esosaosadirector@yahoo.com',
+    'jjeo5191@stu.gusd.net','omolayofoluso@yahoo.com','a.aretief@live.com'
+]
