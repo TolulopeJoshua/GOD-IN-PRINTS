7a0446e5f600930adec97dd5e83d3bac034000af
TolulopeJoshua <babtol235@gmail.com>
2023-04-18 14:21:47 +0100

    t

--- routes/api/gipnews.js
+++ routes/api/gipnews.js
@@ -68,7 +68,8 @@ router.post('/refresh', catchAsync(async (req, res) => {
                         if (!article.image_url && article.content?.length < 1000) return false;
                         if (['cyprus-mail','pakistantoday','manicapost','scripts.24','Snl24','pajhwok','hmetro',
                             'colombopage','sportti','orissapost','allbanaadir','sundaymail','sundaynews',
-                            'coinspeaker','arabnews','docbao','technobaboy','herald.co.zw','jamaica-gleaner'
+                            'coinspeaker','arabnews','docbao','technobaboy','herald.co.zw','jamaica-gleaner',
+                            'thezimbabwean.co'
                             ].filter(src => article.link.includes(src)).length) return false;
                         return (!(sectionData.map(data => data.title).includes(article.title)));
                     }).sort((a,b) => (new Date(b.pubDate) - (new Date(a.pubDate)))).slice(0,1);
