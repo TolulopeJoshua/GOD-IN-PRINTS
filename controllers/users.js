1e8962816c7b5e890c1751b7a020dddd923432f1
TolulopeJoshua <babtol235@gmail.com>
2024-01-02 12:03:46 +0100

    ad margins fix

--- controllers/users.js
+++ controllers/users.js
@@ -103,9 +103,10 @@ module.exports.weeklyMails = async (req, res) => {
   const mails = users
           .filter(user => (new Date() - new Date(user.dateTime) > 7 * 24 * 60 * 60 * 1000) && (!user.preferences.nomail?.set || (new Date() - new Date(user.preferences.nomail?.time) > 90 * 24 * 60 * 60 * 1000)))
           .map(user => user.email).filter(mail => !blockedMails.includes(mail));
-  let index = 0, endIndex = 99 * 4;
+  let index = 99 * 4, endIndex = 99 * 8, sent = 0;
   const interval = setInterval(() => {
     const batch = mails.slice(index, index + 99) 
+    sent += batch.length;
     console.log(batch.length)
     // sendWeeklyMails('babtol235@gmail.com');
     sendWeeklyMails(batch);
@@ -113,10 +114,10 @@ module.exports.weeklyMails = async (req, res) => {
     if (index >= endIndex) clearInterval(interval);
   }, 5000);
   sendPersonalMail({email: 'babtol235@gmail.com', name: 'Josh', subject: 'Weekly Mails Sent', 
-    message:[`Number of users: ${mails.length}`]});
-  res.status(200).send(mails.length.toString());
-  // req.flash('success', `Mails sent successfully - ${mails.length}`);
-  // res.redirect('/profile');
+    message:[`Number of users: ${sent.length}/${users.length}`]});
+  // res.status(200).send(mails.length.toString());
+  req.flash('success', `Mails sent - ${sent.length}/${users.length}`);
+  res.redirect('/profile');
 }
 
 module.exports.getBookReviews = async (req, res) => {
@@ -130,7 +131,7 @@ module.exports.getBookReviews = async (req, res) => {
     const downloads = users[i].downloads.concat(users[i].tktdownloads);
     for (let download of downloads) {
       const daysDiff = new Date() - new Date(download.downloadTime);
-      if ((daysDiff > 14 * 24 * 60 * 60 * 1000) && (daysDiff < 35 * 24 * 60 * 60 * 1000)) {
+      if ((daysDiff > 14 * 24 * 60 * 60 * 1000) && (daysDiff < 28 * 24 * 60 * 60 * 1000)) {
         if (download.bookId && !download.bookId.reviews.find(rev => rev.author._id.toString() == users[i]._id.toString())) {
           console.log(users[i].email, download.bookId.title)
           sendBookReviewsRequest(users[i], download.bookId);
