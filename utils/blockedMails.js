c231c7c93b4abb077f8189f07d2119c28f5ffb2e
TolulopeJoshua <babtol235@gmail.com>
2024-01-12 10:48:16 +0100

    update nomails list

--- utils/blockedMails.js
+++ utils/blockedMails.js
@@ -1,5 +1,8 @@
 module.exports = [
     'tchampion2977@stu.pcssd.org','noelsalvador4@gmail.com','esosaosadirector@yahoo.com',
     'jjeo5191@stu.gusd.net','omolayofoluso@yahoo.com','a.aretief@live.com','emmedge1@gmail.com',
-    'dada.ingen@gmail.com'
+    'dada.ingen@gmail.com', 'glory@gmail.com', 'batalira@yahoo.com', 'joshuva6127@gmail.com',
+    'cljoel65359@gmail.com', 'jacquelines@jdg.co.za', 'mfrqqdxzglqasuqetl@cazlp.com',
+    'rose1711gg@gmail.com', 'haile064@yahoo.com', 'p.wilson28@o2.co.uk', 'godsglorymedia@gmail.com',
+    'havilah2@optusnet.com.au','rn13day@gmail.com','papajay357@gmail.com','rbenners75238@gmail.com',
 ]
