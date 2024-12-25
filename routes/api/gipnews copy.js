06d81bb488fa4374f272142cb112ef347afc4254
Tolulope Babatunde <98036547+TolulopeJoshua@users.noreply.github.com>
2024-12-25 14:55:35 +0000

    news wip

--- routes/api/gipnews copy.js
+++ routes/api/gipnews copy.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../../utils/catchAsync');
 
-const refresher = require('./utils/gipnews-sections-refresher');
-
 const axios = require('axios')
 const { readFileSync, writeFileSync, existsSync, mkdirSync } = require('fs');
 const { v4: uuid } = require('uuid');
@@ -19,14 +17,130 @@ const sects = ['world','sports','business','health','top','science,technology','
     // extract: search extract -- news, media -- first item -- 500
 
 router.post('/refresh', catchAsync(async (req, res) => {
+    // return res.status(200).send('success ' + new Date());
     if (req.headers.id !== process.env.NEXT_SECRET_FIREBASE_APIKEY) {
         return res.status(400).send();
     }
     res.status(200).send('success ' + new Date());
+    let consol = '';
+    try {
+        consol = readFileSync('gipnews.txt') + '\n\n';
+    } catch (e) {
+        writeFileSync('gipnews.txt', consol);
+    }
+    let count = 0, ins = 0;
+    let {section} = req.query;
+    // for (let section of sections) {
+        const options = {
+            method: 'GET',
+            url: 'https://newsdata2.p.rapidapi.com/news',
+            params: {category: section, language: 'en'},
+            headers: {
+                'X-RapidAPI-Key': process.env.NEXT_SECRET_C1,
+                'X-RapidAPI-Host': 'newsdata2.p.rapidapi.com'
+            }
+        };
+        let sectionData = [];
+        const sectionPath = `/tmp/${section.split(',')[0]}.json`;
+        try {
+            if (section == 'reel') {
+                const ids = [17,24,25,28]
+                const urls = ids.map(id => `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=player&part=contentDetails&part=status&chart=mostPopular&maxResults=50&videoCategoryId=${id}&key=${process.env.YOUTUBE_API_KEY}`);
+                const responses = await Promise.all(urls.map(url => axios.get(url))) 
+                let items = responses.reduce((res, sec) => res.concat(sec.data.items), []);
+                sectionData = items.filter(({contentDetails}) => !contentDetails.regionRestriction).map(video => {
+                    const {
+                        id, snippet: {title, description, publishedAt: pubDate, 
+                            thumbnails:{standard}}, player: {embedHtml: content}
+                        } = video;
+                        return ({title, description, content, pubDate, image_url: standard?.url, id});
+                    })
+                consol += (section + ' ' + sectionData.length);
+                writeFileSync(sectionPath, JSON.stringify(sectionData.sort((a,b) => {
+                    return (new Date(b.pubDate) - (new Date(a.pubDate)))
+                }).slice(0,100)));
+            } else {
+                try {
+                    sectionData = JSON.parse(readFileSync(sectionPath)) || [];
+                } catch (error) { 
+                    const url = `https://gipnews-default-rtdb.firebaseio.com/${process.env.NEXT_SECRET_FIREBASE_APIKEY}/${section.split(',')[0]}.json?orderBy="pubDate"&limitToLast=100`
+                    const newsSection = (await axios.get(url)).data;
+                    for (let key in newsSection) {
+                        sectionData.push(newsSection[key]);
+                    }
+                 }
+                let response = await axios.request(options)
+                let { results } = response.data;
+                if (results && results.length) {
+                    results = results.filter(article => {
+                        if (!(article.description || article.content)) return false;
+                        if (!article.image_url && article.content?.length < 1000) return false;
+                        if (['cyprus-mail','pakistantoday','manicapost','scripts.24','Snl24','pajhwok','hmetro',
+                            'colombopage','sportti','orissapost','allbanaadir','sundaymail','sundaynews',
+                            'coinspeaker','arabnews','docbao','technobaboy','herald.co.zw','jamaica-gleaner',
+                            'thezimbabwean.co','fiji','prnewswire','thepeninsulaqatar','247newsaroundtheworld',
+                            'bulawayo24'
+                            ].filter(src => article.link.includes(src)).length) return false;
+                        return (!(sectionData.map(data => data.title).includes(article.title)));
+                    }).sort((a,b) => (new Date(b.pubDate) - (new Date(a.pubDate)))).slice(0,1);
+                    consol += (section + ' ' + results.length) + ' ';
+                    console.log(results);
+                    for (let result of results) {
+                        let {title, link, description, content, pubDate, image_url, id} = result;
+                        description = description || content.slice(0,100);
+                        content = content || description;
+                        id = uuid();
+                        if (count < 2) {
+                            const key = [
+                                process.env.NEXT_SECRET_C1, process.env.NEXT_SECRET_C2,
+                                process.env.NEXT_SECRET_C3, process.env.NEXT_SECRET_C4,
+                            ][sects.indexOf(section) % 4]
+                            const options = {
+                                method: 'GET',
+                                url: 'https://extract-news.p.rapidapi.com/v0/article',
+                                params: { url: link },
+                                headers: {
+                                    'X-RapidAPI-Key': key,
+                                    'X-RapidAPI-Host': 'extract-news.p.rapidapi.com'
+                                }
+                            };
+                            consol += (link.split('/')[2]) + ' - ';
+                            consol += (title);
+                            response = await axios.request(options)
+                            const {article} = response.data;
+                            if (article) {
+                                content = article.text || content; image_url = article.meta_image || image_url; 
+                                description = article.meta_description || description;
+                            }
+                            const dbPath = `https://gipnews-default-rtdb.firebaseio.com/${process.env.NEXT_SECRET_FIREBASE_APIKEY}/${section.split(',')[0]}/${id}.json`
+                            await axios.put(dbPath, JSON.stringify({title, link, description, content, pubDate, image_url, id}))
+                            sectionData.unshift({title, link, description, content, pubDate, image_url, id});
+                            writeFileSync(sectionPath, JSON.stringify(sectionData.sort((a,b) => {
+                                return (new Date(b.pubDate) - (new Date(a.pubDate)))
+                            }).sort((a,b) => {
+                                const val = (a.image_url && !b.image_url) ? -1 :
+                                            (b.image_url && !a.image_url) ? 1 : 0
+                                return val;
+                            }).slice(0,100))); 
+                            consol += '\n' + (`${ins += 1} - ${section}`)
+                            
+                            let xmap = '';
+                            try {
+                                xmap = readFileSync('gipXmap.xml');
+                            } catch (error) { }
+                            xmap += `<url>\n\ \ <loc>https://gipnews.vercel.app/${section}/${id}?title=${encodeURI(title.replace(/[\ \/\?\:\;\,\.\|]/g, '-'))}</loc>\n\ \ <lastmod>${(new Date()).toISOString()}</lastmod>\n\ \ <priority>0.64</priority>\n</url>\n`
+                            writeFileSync('gipXmap.xml', xmap);
 
-    let { section } = req.query;
-    section = section.split(',')[0];
-    await refresher[section]();
+                            count += 1;
+                        } else {
+                            console.log('Escaped insertion: ' + section)
+                        }
+                    }
+                }
+            }
+        } catch (error) { consol += (error) }
+        // writeFileSync('gipnews.txt', consol);
+    // }
 }))
 
 router.post('/refresh2', catchAsync(async (req, res) => {
@@ -137,9 +251,9 @@ router.get('/data', catchAsync(async (req, res) => {
             let sectionData = [];
             let sectionPath = `/tmp/${section?.split(',')[0]}.json`;
             try {
-                throw ' '
-                // sectionData = JSON.parse(readFileSync(sectionPath)) || [];
-                // data[section] = sectionData.map(art => ({...art, section}));
+                // throw ' '
+                sectionData = JSON.parse(readFileSync(sectionPath)) || [];
+                data[section] = sectionData.map(art => ({...art, section}));
             } catch (error) { 
                 console.log('db get');
                 const urls = sects.slice(0,7).map(sec => `https://gipnews-default-rtdb.firebaseio.com/${process.env.NEXT_SECRET_FIREBASE_APIKEY}/${sec.split(',')[0]}.json?orderBy="pubDate"&limitToLast=100`)
@@ -163,9 +277,9 @@ router.get('/data', catchAsync(async (req, res) => {
                                     (b.image_url && !a.image_url) ? 1 : 0
                         return val;
                     }).map(art => ({...art, section}));
-                    // try {
-                    //     writeFileSync(sectionPath, JSON.stringify(data[section])); 
-                    // } catch (error) { console.log('could not write to file') }
+                    try {
+                        writeFileSync(sectionPath, JSON.stringify(data[section])); 
+                    } catch (error) { console.log('could not write to file') }
                 }
                 section = 'reel';
                 sectionPath = `/tmp/reel.json`;
@@ -182,9 +296,9 @@ router.get('/data', catchAsync(async (req, res) => {
                 data[section] = sectionData.sort((a,b) => {
                     return (new Date(b.pubDate) - (new Date(a.pubDate)))
                 }).slice(0,100).map(art => ({...art, section}));
-                // try {
-                //     writeFileSync(sectionPath, JSON.stringify(data[section]));   
-                // } catch (error) { console.log('could not write to file') }
+                try {
+                    writeFileSync(sectionPath, JSON.stringify(data[section]));   
+                } catch (error) { console.log('could not write to file') }
                 break;
             }
         }
@@ -201,9 +315,8 @@ router.get('/:section', catchAsync(async (req, res) => {
     const { section } = req.params
     let sectionPath = `/tmp/${section.split(',')[0]}.json`;
     try {
-        throw ' ';
-        // sectionData = JSON.parse(readFileSync(sectionPath)) || [];
-        // sectionData = sectionData.map(art => ({...art, section}));
+        sectionData = JSON.parse(readFileSync(sectionPath)) || [];
+        sectionData = sectionData.map(art => ({...art, section}));
     } catch (error) { 
         if (section == 'reel') {
             const urls = [17,24,25,28].map(id => `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=player&part=contentDetails&part=status&chart=mostPopular&maxResults=50&videoCategoryId=${id}&key=${process.env.YOUTUBE_API_KEY}`)
@@ -229,9 +342,9 @@ router.get('/:section', catchAsync(async (req, res) => {
                         (b.image_url && !a.image_url) ? 1 : 0
             return val;
         }).slice(0,100).map(art => ({...art, section}));
-        // try {
-        //     writeFileSync(sectionPath, JSON.stringify(sectionData)); 
-        // } catch (error) { console.log('could not write to file') }
+        try {
+            writeFileSync(sectionPath, JSON.stringify(sectionData)); 
+        } catch (error) { console.log('could not write to file') }
      }
     const features = sects.filter(sect => sect != section).map(section => {
         sectionPath = `/tmp/${section.split(',')[0]}.json`;
@@ -263,9 +376,8 @@ router.get('/:section/:id', catchAsync(async (req, res) => {
         const { section, id } = req.params
         const sectionPath = `/tmp/${section?.split(',')[0]}.json`;
         try {
-            throw '';
-            // sectionData = JSON.parse(readFileSync(sectionPath)) || [];
-            // sectionData = sectionData.map(art => ({...art, section}));
+            sectionData = JSON.parse(readFileSync(sectionPath)) || [];
+            sectionData = sectionData.map(art => ({...art, section}));
         } catch (error) {
             // if (section == 'reel') {
             //     const urls = [17,24,25,28].map(id => `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=player&part=contentDetails&part=status&chart=mostPopular&maxResults=50&videoCategoryId=${id}&key=${process.env.YOUTUBE_API_KEY}`)
---
+++ routes/api/utils/gipnews-sections-refresher.js
@@ -0,0 +1,218 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const axios = require("axios");
+
+const options = (url, key, otherParams = {}) => ({
+  method: "GET",
+  url,
+  params: { languages: "en", ...otherParams },
+  headers: {
+    "X-RapidAPI-Key": process.env.NEXT_SECRET_C1, // key,
+    "X-RapidAPI-Host": url.slice(8).split(".com")[0] + ".com",
+  },
+});
+
+const dbUrl = (section) =>
+  `https://gipnews-default-rtdb.firebaseio.com/${process.env.NEXT_SECRET_FIREBASE_APIKEY}/${section}.json?`;
+
+const getSection = async (section) => {
+  const url = dbUrl(section);
+  const { data } = await axios.get(url);
+  return Object.values(data).sort(
+    (a, b) => new Date(b.pubDate) - new Date(a.pubDate)
+  );
+};
+
+const putSection = async (section, news) => {
+  const url = dbUrl(section);
+  await axios.put(url, Object.fromEntries(news.map((n) => [n.id, n])));
+};
+
+const extract = async (urls, key) => {
+  const eUrl = "https://extract-news.p.rapidapi.com/v0/article";
+
+  const news = [];
+  for (const url of urls) {
+    try {
+      const {
+        data: { article },
+      } = await axios(options(eUrl, key, { url }));
+      if (!(article.meta_image && (article.meta_description || article.text))) {
+        continue;
+      }
+      news.push({
+        title: article.title,
+        link: url,
+        description: article.meta_description,
+        content: article.text,
+        pubDate: article.published || new Date().toISOString(),
+        image_url: article.meta_image,
+        id: Date.now(),
+      });
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  }
+  return news;
+};
+
+const getWorld = async () => {
+  const url = "https://news67.p.rapidapi.com/v2/trending";
+  const key = process.env.NEXT_SECRET_C1;
+
+  const old = await getSection("world");
+
+  const { data } = await axios(options(url, key));
+  const links = data.news
+    .map((cluster) => cluster.News)
+    .flat()
+    .sort((a, b) => new Date(b.PublishedOn) - new Date(a.PublishedOn))
+    .filter((news) => !old.some((o) => o.link === news.Url))
+    .map((news) => news.Url)
+    .slice(0, 5);
+
+  const news = await extract(links, key);
+  await putSection("world", [...news, ...old].slice(0, 100));
+};
+
+const getSports = async () => {
+  const url = "https://sportsdaily.p.rapidapi.com/api/sports/news/";
+  const key = process.env.NEXT_SECRET_C3;
+
+  const old = await getSection("sports");
+
+  const { data } = await axios(options(url, key, { language: "en" }));
+  const links = data.results
+    .sort((a, b) => new Date(b.published) - new Date(a.published))
+    .filter((news) => !old.some((o) => o.link === news.source_url))
+    .map((news) => news.source_url)
+    .slice(0, 5);
+
+  const news = await extract(links, key);
+  await putSection("sports", [...news, ...old].slice(0, 100));
+};
+// getSports();
+
+const getBusiness = async () => {
+  const url = "https://biztoc.p.rapidapi.com/news/source/bbc";
+  const key = process.env.NEXT_SECRET_C2;
+
+  const old = await getSection("business");
+
+  const { data } = await axios(options(url, key, { language: "en" }));
+  const links = data
+    .sort((a, b) => new Date(b.published) - new Date(a.published))
+    .filter((news) => !old.some((o) => o.link === news.url))
+    .map((news) => news.url)
+    .slice(0, 5);
+
+  const news = await extract(links, key);
+  await putSection("business", [...news, ...old].slice(0, 100));
+};
+
+const getHealth = async () => {
+  const url = "https://newsapi90.p.rapidapi.com/topic/health";
+  const key = process.env.NEXT_SECRET_C4;
+
+  const old = await getSection("health");
+
+  const { data } = await axios(options(url, key, { language: "en" }));
+  const links = data
+    .sort((a, b) => new Date(b.date) - new Date(a.date))
+    .filter((news) => !old.some((o) => o.link === news.link))
+    .map((news) => news.link)
+    .slice(0, 5);
+
+  const news = await extract(links, key);
+  await putSection("health", [...news, ...old].slice(0, 100));
+};
+
+const getTop = async () => {
+  const url = "https://news67.p.rapidapi.com/v2/feed";
+  const key = process.env.NEXT_SECRET_C1;
+
+  const old = await getSection("top");
+
+  const { data } = await axios(options(url, key));
+  const links = data.news
+    .sort((a, b) => new Date(b.PublishedOn) - new Date(a.PublishedOn))
+    .filter((news) => !old.some((o) => o.link === news.Url))
+    .map((news) => news.Url)
+    .slice(0, 5);
+
+  const news = await extract(links, key);
+  await putSection("top", [...news, ...old].slice(0, 100));
+};
+
+const getScience = async () => {
+  const url = "https://technology-news3.p.rapidapi.com/news";
+  const key = process.env.NEXT_SECRET_C2;
+
+  const old = await getSection("science");
+
+  const { data } = await axios(options(url, key, { language: "en" }));
+  const links = data
+    .filter((news) => !old.some((o) => o.link === news.url))
+    .map((news) => news.url)
+    .slice(0, 5);
+
+  const news = await extract(links, key);
+  await putSection("science", [...news, ...old].slice(0, 100));
+};
+
+const getEntertainment = async () => {
+  const url = "https://cnn-api1.p.rapidapi.com/category";
+  const key = process.env.NEXT_SECRET_C3;
+
+  const old = await getSection("entertainment");
+
+  const { data } = await axios(
+    options(url, key, { url: "https://edition.cnn.com/entertainment" })
+  );
+  const links = data
+    .filter((news) => !old.some((o) => o.link === news.link))
+    .map((news) => news.link)
+    .slice(0, 5);
+
+  const news = await extract(links, key);
+  await putSection("entertainment", [...news, ...old].slice(0, 100));
+};
+
+const getReel = async () => {
+  const ids = [17, 24, 25, 28];
+  const urls = ids.map(
+    (id) =>
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=player&part=contentDetails&part=status&chart=mostPopular&maxResults=50&videoCategoryId=${id}&key=${process.env.YOUTUBE_API_KEY}`
+  );
+
+  const responses = await Promise.all(urls.map((url) => axios.get(url)));
+  let items = responses.map((res) => res.data.items).flat();
+  items = items
+    .filter(({ contentDetails }) => !contentDetails.regionRestriction)
+    .map((video) => ({
+      id: video.id,
+      title: video.snippet.title,
+      description: video.snippet.description,
+      content: video.player.embedHtml,
+      pubDate: video.snippet.publishedAt,
+      image_url: video.snippet.thumbnails.standard?.url,
+    }))
+    .sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate))
+    .slice(0, 100);
+
+  await putSection("reel", items);
+};
+
+
+module.exports = {
+  world: getWorld,
+  sports: getSports,
+  business: getBusiness,
+  health: getHealth,
+  top: getTop,
+  science: getScience,
+  entertainment: getEntertainment,
+  reel: getReel,
+}
