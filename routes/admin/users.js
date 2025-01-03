84f84735dc138395f8e4db09d0e5df276cca76e4
e4ewebdev <webdev@e4email.net>
2025-01-03 10:25:56 +0100

    adm mng subscription

--- routes/admin/users.js
+++ routes/admin/users.js
@@ -24,7 +24,7 @@ router.get('/subscription', isAdmin, catchAsync(async (req, res) => {
         users = await User.aggregate([
             { 
                 $match: { 
-                    'subscription.status.type': subscription,
+                    'subscription.status': subscription,
                 } 
             },
             { $skip: 0 },
